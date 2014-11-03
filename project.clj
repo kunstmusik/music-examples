9138c09361e8ce7ba2ba6c2ca185f83983b9a4e1
Steven Yi <stevenyi@gmail.com>
2014-11-03 13:03:34 -0500

    updated for settings used with Pink (G1, etc.)

--- project.clj
+++ project.clj
@@ -4,20 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;:jvm-opts ^:replace  []
-  :jvm-opts 
+  :jvm-opts ;["-server" "-Xmx2g" "-XX:-UseParallelGC"]
   ^:replace
   ["-server"
-   "-Xms256m" "-Xmx1g"           ; Minimum and maximum sizes of the heap
+   "-Xmx512m"           ; Minimum and maximum sizes of the heap
    "-XX:+UseG1GC"
-   "-XX:MaxGCPauseMillis=10"     ; Specify a target of 20ms for max gc pauses
-   "-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
-   "-XX:NewSize=256m"            ;  generation to be small
+   "-XX:MaxGCPauseMillis=1"     ; Specify a target of 20ms for max gc pauses
+   ;"-XX:MaxNewSize=257m"         ; Specify the max and min size of the new
+   ;"-XX:NewSize=256m"            ;  generation to be small
    "-XX:+UseTLAB"                ; Uses thread-local object allocation blocks. This
    ;  improves concurrency by reducing contention on
    ;  the shared heap lock.
-   "-XX:MaxTenuringThreshold=0" ; Makes the full NewSize available to every NewGC 
-   "-Xshare:off" 
-   ] 
+   ;"-XX:MaxTenuringThreshold=0" ; Makes the full NewSize available to every NewGC 
+
+   ; for GC diagnostics 
+   ;"-XX:+PrintGCDetails"
+   ;"-XX:+PrintGCTimeStamps"
+   ]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [kunstmusik/pink "0.1.0-SNAPSHOT"]
