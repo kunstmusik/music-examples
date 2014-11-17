5cf54bc59fdc93c4f4a5ee953ccc33975b0df1f6
Steven Yi <stevenyi@gmail.com>
2014-11-16 19:23:27 -0500

    corrected fm synth, made patches selectable by knob

--- src/music_examples/example1aa.clj
+++ src/music_examples/example1aa.clj
@@ -2,10 +2,12 @@
   (:require [pink.simple :refer :all] 
             [pink.io.midi :refer :all]
             [pink.config :refer :all]
+            [pink.node :refer :all]
             [pink.space :refer :all]
             [pink.oscillators :refer :all]
             [pink.envelopes :refer [env xar adsr]]
             [pink.filters :refer [port butterlp moogladder]]
+            [pink.delays :refer :all]
             [pink.util :refer :all]
             [primitive-math :refer [not==]]
             )
@@ -20,6 +22,7 @@
 (def midim (create-midi-manager))
 (def keyboard (add-virtual-device midim "keyboard 1")) 
 
+;; Setup reader functions for MIDI Control Change values
 (defn midi-atom-reader
   "Read from atom assigned to MIDI CC values in Pink MIDI System.
   Scales values into target min and max range."
@@ -36,31 +39,33 @@
       out)))
 
 
-(def cutoff-freq (get-midi-cc-atom keyboard 0 1))
+(def patch-number (get-midi-cc-atom keyboard 0 1))
 (def cutoff-depth (get-midi-cc-atom keyboard 0 2))
 (def resonance (get-midi-cc-atom keyboard 0 3))
 
-(def cfreq-fn (shared (port (midi-atom-reader cutoff-freq 0.0 1.0) 0.05)) )
-(def cdepth-fn (shared (port (midi-atom-reader cutoff-depth 0.0 16.0) 0.05)) )
+(def cdepth-fn (shared (port (midi-atom-reader cutoff-depth 0.0 16.0) 0.05)))
 (def resonance-fn (shared (port (midi-atom-reader resonance 0.0 1.0) 0.05)))
 
 
 ;; Synthesizer Note Instance Function
-(defn saw
-  [freq amp]
-  (let [ampfn (mul amp (adsr 0.05 0.3 0.9 4.0))] 
-    (let-s [f (sum freq (mul freq 0.0025 (sine 4)))] 
-      (-> (moogladder 
-                  (sum (blit-saw (mul f 1.5)) 
-                       (blit-square f) 
-                       (blit-square (mul f 0.9995))) 
-                  (sum freq (mul cdepth-fn freq ampfn))    
-                  resonance-fn     
-                  )
-          (mul ampfn) 
-          (pan 0.0)
-          )))) 
+;; Frequency Modulation
+(defn fm
+  "Simple frequency-modulation sound with 2:1 cm ratio"
+  [^double freq amp]
+  (let [fm-index 0.9 
+        mod-mult 1.0 
+        mod-freq (* freq mod-mult)
+        ]
+    (let-s [e (adsr 0.04 0.03 0.9 3.0)] 
+    (->
+      (sine2 (sum freq (mul freq fm-index e 
+                            (sine2 mod-freq))))
+      (mul amp e)
+      (pan 0.0)
+      ))))
 
+
+;; Additive Synthesis
 (defn additive-filter 
   [cutoff freq]
   (if (< freq cutoff)
@@ -83,39 +88,89 @@
       (mul amp (adsr 0.05 0.3 0.9 4.0) (/ 3.0 num-partials))
       (pan 0.0))))
 
-(defn fm
-  "Simple frequency-modulation sound with 2:1 cm ratio"
+;; Subtractive Synthesis
+(defn saw
   [freq amp]
-  (let-s [e (adsr 0.0001 0.0 1.0 9.0)] 
-    (->
-      (sine2 (sum freq (mul 0.5 e (sine2 (* 2.0 freq)))))
-      (mul amp 0.25 e)
-      (pan 0.0)
-      )))
+  (let [ampfn (mul amp (adsr 0.05 0.3 0.9 4.0))] 
+    (let-s [f (sum freq (mul freq 0.0025 (sine 4)))] 
+      (-> 
+        (sum (blit-saw (mul f 1.5)) 
+             (blit-square f) 
+             (blit-square (mul f 0.9995)))  
+        (moogladder 
+          (sum freq (mul cdepth-fn freq ampfn)) resonance-fn)
+        (mul ampfn) 
+        (pan 0.0)
+        )))) 
 
+
+;; Effects
+(defn ping-pong-delay
+  "Creates a stereo ping-pong delay given a mono audio function."
+  [afn left-delay-time left-amp-mod
+   right-delay-time right-amp-mod]
+  (let [^"[[D" out (create-buffers 2) 
+         ain (shared afn) 
+         lfeedback (create-buffer 0.0)
+         rfeedback (create-buffer 0.0)
+         ldelay (->
+                  (feedback-read lfeedback)
+                  (mul left-amp-mod) 
+                  (sum ain)
+                  (adelay left-delay-time)
+                  (feedback-write lfeedback))
+         rdelay  (->
+                  (feedback-read rfeedback)
+                  (mul right-amp-mod) 
+                  (sum ain)
+                  (adelay right-delay-time)
+                  (feedback-write rfeedback))] 
+    (fn []
+      (let [aleft (ldelay)
+            aright (rdelay)]
+        (when (and aleft aright)
+          (aset out 0 aleft) 
+          (aset out 1 aright) 
+          out)))))
+
+;; Bind hardware device to virtual MIDI device
   (bind-device midim "MPKmini2" "keyboard 1")
 
-  (bind-key-func
-    keyboard 0
-    (let [active ^"[[Z" (make-array Boolean/TYPE 128 1)] 
-      (fn [cmd note-num velocity]
-        (condp = cmd
-          ShortMessage/NOTE_ON
-          (let [done (boolean-array 1 false)
-                afn (binding [*done* done] 
-                      ;(additive (midi->freq note-num) (/ (double velocity) 127.0)))]
-                      (saw (midi->freq note-num) (/ (double velocity) 127.0)))]
-                      ;(fm (midi->freq note-num) (/ (double velocity) 127.0)))]
-            (aset active note-num done)
-            (add-afunc afn))                        ;; <= add-afunc
+;; Create vector of patches to choose from
+  (def patches 
+    [fm additive saw])
 
+;; setup ping-pong delay graph
+  (def delay-node  (create-node))
+  (def delay-processor (shared (node-processor delay-node)))
 
+  (add-afunc (pan delay-processor 0.0))
+  (add-afunc (ping-pong-delay delay-processor 
+                               0.57 0.9 0.43 0.8))
 
-          ShortMessage/NOTE_OFF
-          (when-let [^booleans done (aget active note-num)]
-            (aset done 0 true)
-            (aset active note-num nil)))
-        )))
+(bind-key-func
+  keyboard 0
+  (let [active ^"[[Z" (make-array Boolean/TYPE 128 1)] 
+    (fn [cmd note-num velocity]
+      (condp = cmd
+        ShortMessage/NOTE_ON
+        (let [done (boolean-array 1 false)
+              patch-num (min 2 (/ (long @patch-number) 30)) 
+              patch-fn (nth patches patch-num)
+              afn (binding [*done* done] 
+                    (patch-fn (midi->freq note-num) 
+                           (/ (double velocity) 127.0)))]
+          (aset active note-num done)
+          (add-afunc afn)                         ;; <= add-afunc 
+          ;(node-add-func delay-node afn) 
+          
+          )
+
+        ShortMessage/NOTE_OFF
+        (when-let [^booleans done (aget active note-num)]
+          (aset done 0 true)
+          (aset active note-num nil)))
+      )))
 
 ;; Start engine for performance
 (start-engine)
