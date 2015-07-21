b576814f9cb9436758928ac5ea0a838654e035cd
Steven Yi <stevenyi@gmail.com>
2015-07-21 14:24:08 -0400

    updated sieves example to demonstrate routing signals between multiple nodes (reverb)

--- src/music_examples/sieves.clj
+++ src/music_examples/sieves.clj
@@ -28,49 +28,64 @@
    (fm freq amp 0.4 1.77))
   ([freq amp fm-index mod-mult]
   (let [mod-freq (mul freq mod-mult)]
-    (let-s [e (adsr 0.02 2.0 0.0 0.0)] 
+    (let-s [e (adsr 0.02 2.0 0.0 0.01)] 
       (->
         (sine2 (sum freq (mul freq fm-index e 
                               (sine2 mod-freq))))
         (mul amp e)
-        (pan 0.0)
+        ;(pan 0.0)
         )))))
 
 
 (defn ringm 
   "Simple instrument with ring-modulation"
   ([freq amp]
-   (let-s [e (adsr 1.0 2.0 0.0 0.0)] 
+   (let-s [e (adsr 0.04 2.0 0.0 0.01)] 
      (->
        (ringmod 
          (blit-saw freq)
          (sine2 (mul freq 2.0)))
        (mul amp e)
-       (pan 0.0)
+       ;(pan 0.0)
        ))))
 
-
-;; ================= 
 ;; AUDIO GRAPH SETUP 
 ;; =================
 
-(def dry-node (create-node))
-(def reverb-node (create-node))
+(def dry-node (create-node :channels 2))
+(def reverb-node (create-node :channels 2))
 
 (add-afunc (node-processor dry-node))
-(add-afunc (node-processor reverb-node))
-
+(add-afunc (freeverb (node-processor reverb-node)
+                    0.9 0.5))
+(defn clear-afns []
+  (node-clear dry-node)
+  (node-clear reverb-node))
 
 ;; =====================
 ;; PERFORMANCE FUNCTIONS 
 ;; =====================
 
-(defn perf-fm [afn]
-  (let-s [sig (pan afn 0.05)] 
+(defn mix-afn [afn]
+  (let-s [sig (pan afn 0.2)] 
     (node-add-func
-      dry-node (apply-stereo mul sig 0.7))
+      dry-node 
+      (apply-stereo mul sig 0.7))
     (node-add-func
-      reverb-node (apply-stereo mul sig 0.3))))
+      reverb-node 
+      (apply-stereo mul sig 0.3)))
+  nil)
+
+(defn perf-fm [dur & args] 
+  (binding [*duration* dur] 
+    (mix-afn (apply fm args))))
+
+(defn perf-ringm [dur & args]
+  (binding [*duration* dur] 
+    (mix-afn (apply ringm args))))
+
+(defn s [afn start dur & args]
+  (event #(apply afn dur args) start ))
 
 ;; ===== 
 ;; SCORE
@@ -78,7 +93,7 @@
 
 (defn sieve-chord 
   ([base-pch sieve dur amp]
-   (sieve-chord perf-fm base-pch sieve dur amp))
+   (sieve-chord perf-ringm base-pch sieve dur amp))
   ([instrfn base-pch sieve dur amp]
   (gen-notes 
     (repeat instrfn)
@@ -92,18 +107,21 @@
 (def score
   [:meter 4 4
 
-  0.0 (sieve-chord [8 0] (gen-sieve 7 [2 0]) 3.0 0.25) 
-  0.5 (sieve-chord [8 3] (gen-sieve 7 [2 0]) 3.0 0.25) 
+  0.0 (sieve-chord [8 0] (gen-sieve 7 [2 0]) 1.0 0.25) 
+  0.25 (sieve-chord [8 3] (gen-sieve 7 [2 0]) 3.0 0.25) 
 
-  1.0 (sieve-chord [8 0] (gen-sieve 7 (U [4 0] [3 1])) 3.0 0.10) 
-  1.1 (sieve-chord [8 3] (gen-sieve 7 (U [2 0] [3 1])) 3.0 0.10) 
-  1.2 (sieve-chord [8 2] (gen-sieve 7 (U [3 0] [4 1])) 3.0 0.10) 
-  1.3 (sieve-chord [8 1] (gen-sieve 7 (U [3 2] [4 0])) 3.0 0.10) 
-  1.4 (sieve-chord [7 10] (gen-sieve 7 (U [3 0] [4 1])) 3.0 0.10) 
-  1.5 (sieve-chord [7 8] (gen-sieve 7 (U [2 0] [3 1])) 3.0 0.10) 
-  1.6 (sieve-chord [7 5] (gen-sieve 7 (U [4 0] [3 3] [11 5])) 3.0 0.10) 
+  1.0 (sieve-chord [9 0] (gen-sieve 7 (U [4 0] [3 1])) 1.0 0.25) 
+  1.25 (sieve-chord [7 3] (gen-sieve 7 (U [4 0] [3 1])) 3.0 0.25) 
 
-  ;2.0 (sieve-chord ringm [8 3] (gen-sieve 7 [2 0]) 8.0 0.05) 
+  ;1.0 (sieve-chord [8 0] (gen-sieve 7 (U [4 0] [3 1])) 3.0 0.10) 
+  ;1.1 (sieve-chord [8 3] (gen-sieve 7 (U [2 0] [3 1])) 3.0 0.10) 
+  ;1.2 (sieve-chord [8 2] (gen-sieve 7 (U [3 0] [4 1])) 3.0 0.10) 
+  ;1.3 (sieve-chord [8 1] (gen-sieve 7 (U [3 2] [4 0])) 3.0 0.10) 
+  ;1.4 (sieve-chord [7 10] (gen-sieve 7 (U [3 0] [4 1])) 3.0 0.10) 
+  ;1.5 (sieve-chord [7 8] (gen-sieve 7 (U [2 0] [3 1])) 3.0 0.10) 
+  ;1.6 (sieve-chord [7 5] (gen-sieve 7 (U [4 0] [3 3] [11 5])) 3.0 0.10) 
+
+  2.0 (sieve-chord [8 3] (gen-sieve 7 [2 0]) 8.0 0.05) 
 
  ])
 
@@ -117,8 +135,8 @@
   (->>
     (convert-measured-score score)
     (starting-at (* measure 4))
-    ;(sco->events)
-    ;(add-audio-events)
+    (map #(apply s %))
+    (add-events)
     ))
 
 (defn play-block [block]
